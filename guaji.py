5b95a7662f237eb7bfc85af8ed91e9faf9ef378f
liujun2 <liujun@wyze.cn>
2020-07-28 21:09:10 +0800

    for all course

--- guaji.py
+++ guaji.py
@@ -7,35 +7,30 @@ import sys
 path = '/usr/local/bin/chromedriver' #your chromedriver path
 sys.path.append(path)
 
-driver = webdriver.Chrome()
-driver.get("https://www.bjjnts.cn/") 
-time.sleep(60)
+def conform_dialog():
+    elements = driver.find_elements_by_class_name('layui-layer-btn0')
+    if len(elements) == 0:
+        print('no dialog')
+    else:
+        print('conform')
+        for ele in elements:
+            ele.click()
 
-import pandas as pd
-data = pd.read_csv('course_1.csv', header = None)
-print(type(data))
-for index, row in data.iterrows():
-    chapter_name = row[0].split(' ')[0]
-    chapter_time = int(row[1].split(':')[0])*3600 + int(row[1].split(':')[1])*60 + int(row[1].split(':')[2])
-    print(index,chapter_name,chapter_time)
-    chapter_index = int(row[0].split(' ')[1].split('-')[1]) - 1
-    print(chapter_index)
-    elements = driver.find_elements_by_class_name(chapter_name)
-    print(elements)
-    if len(elements) > 0:
-        elements[chapter_index].click()
-    count_down_number = chapter_time + 60
-    while (count_down_number > 0):
-        print('倒计时',count_down_number)
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    driver.get("https://www.bjjnts.cn/login") 
+    time.sleep(60)
+    num_course_unlock = 0
+
+    while True:
         time.sleep(1)
-        try:
-            elements = driver.find_elements_by_class_name('layui-layer-btn0')
-            if len(elements) == 0:
-                print('no dialog')
-            else:
-                print('elements',elements)
-                for ele in elements:
-                    ele.click()
-        except Exception as e:
-            print(e)
-        count_down_number -= 1
+        conform_dialog()
+        time.sleep(1)
+        elements = driver.find_elements_by_class_name('change_chapter')
+        # print(len(elements))
+        elements_un_lock = list(filter(lambda element : int(element.get_attribute('data-lock')) == 0, elements))
+        # print(len(elements_un_lock))
+        if len(elements_un_lock) != num_course_unlock:
+            num_course_unlock = len(elements_un_lock)
+            elements_un_lock[len(elements_un_lock) - 1].click()
+
